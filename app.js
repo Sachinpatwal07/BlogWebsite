b5a664346572a4cd236de5bac437f130b1c2a495
sachinpatwal07 <sachinpatwal078@gmail.com>
2021-05-24 12:27:53 +0530

    intial commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+const ellipsis = require('text-ellipsis');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -23,8 +25,10 @@ let posts = [];
 app.get("/", function(req, res) {
 
   res.render("home", {
-    startingContent: homeStartingContent
+    startingContent: homeStartingContent,
+    posts: posts
   });
+
 
 });
 
@@ -51,6 +55,38 @@ app.get("/compose", function(req, res) {
 
 });
 
+
+app.get("/posts/:par_name", function(req, res) {
+
+
+
+
+
+
+  const requireTitle = _.lowerCase(req.params.par_name);
+
+
+  posts.forEach(function(post) {
+
+    const storetitle = _.lowerCase(post.title);
+
+    if (storetitle === requireTitle)
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+
+  });
+
+
+
+
+
+});
+
+
+
+
 app.post("/compose", function(req, res) {
 
 
@@ -62,7 +98,6 @@ app.post("/compose", function(req, res) {
 
   }
   posts.push(post);
-  console.log(posts);
   res.redirect("/");
 
 
